ae14fe4be8a741b477d8370194b5ce801130756a
MariliaDamasceno <139273477+MariliaDamasceno@users.noreply.github.com>
2023-11-14 20:10:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,32 +12,10 @@ app.use(express.json());//Diz ao aplicativo Express para usar o middleware JSON
 
 app.use('/users', routes)
 
-
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
-
-app.get("/teste-token", (req, res) => {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-        return res.status(401).send({ message: "O token não foi informado" });
-    }
-    
-    const [scheme, token] = authHeader.split(" ");
-    
-    if (!scheme || !/^Bearer$/i.test(scheme) || !token) {
-        return res.status(401).send({ message: "O token está malformatado" });
-    }
-    
-    jwt.verify(token, segredo, async (err, decoder) => {
-        if (err) {
-            return res.status(401).send({ message: "Falha ao verificar o token" });
-        }
-    
-       res.send(decoder);
-    });
-    
+ 
 })
 
 app.listen(port, () => {
